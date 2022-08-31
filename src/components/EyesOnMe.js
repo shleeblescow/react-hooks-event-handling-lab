2546d3373f768c2c28dcf7afd53ef24907bcff62
Shelby Lee <slee337@uw.edu>
2022-08-31 15:49:05 -0600

    commit tree e7fc9ab14463965c7183ce8b0c8ef0ca86ad0580

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+
+
+// In that component, render a button with the text 'Eyes on me'.
+// On that button, add event handlers that listen for the focus and blur events.
+// When the focus event fires, use console.log to print out the text 'Good!'.
+// When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
+// import React from 'react'
+
+function EyesOnMe() {
+    
+    function handleFocus() {
+        console.log("Good!")
+    }
+
+    function handleBlur() {
+        console.log("Hey! Eyes on me!")
+    }
+    
+    return(
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,20 @@
 // Code Keypad Component Here
+// <input type="password" /> to capture input
+
+// render an input with the right type.
+// add an event handler that listens for the change event on that input
+// print out the text 'Entering password...' when that event fires
+
+import React from 'react'
 
 function Keypad (){
+
+    function handleClick() {
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <input type='password' onChange={handleClick} />
     )
 }
 
